c373b929e6c95d66cc662c6257a3aafcf7a48712
Eugene Tye <eugenetye123@gmail.com>
2023-09-04 18:51:51 -0400

    added custom fonts

--- src/Discover.tsx
+++ src/Discover.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font';
 
 export const Discover = () => {
+  const [fontsLoaded] = useFonts({
+    'Questrial-Regular': require('../assets/fonts/Questrial-Regular.ttf'),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View>
-      <Text className='text-4xl p-4'>Discover</Text>
+      <Text style={{ fontFamily: 'Questrial-Regular', fontSize: 40, padding:15 }}>Discover</Text>
     </View>
   )
 }
--- src/Trails.tsx
+++ src/Trails.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font';
 
 export const Trails = () => {
+  const [fontsLoaded] = useFonts({
+    'Questrial-Regular': require('../assets/fonts/Questrial-Regular.ttf'),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View>
-      <Text className='text-4xl p-4'>Trails</Text>
+      <Text style={{ fontFamily: 'Questrial-Regular', fontSize: 40, padding:15 }}>Trails</Text>
     </View>
   )
 }
