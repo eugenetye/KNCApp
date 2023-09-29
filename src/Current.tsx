29b1db6667e8d0c9e90ad2c87bd441e750122226
ErnestAu <ernestau7@gmail.com>
2023-09-29 11:00:36 -0400

    made updates to current.tsx

--- src/Current.tsx
+++ src/Current.tsx
@@ -1,20 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, {useEffect, useState} from 'react'
 import { useFonts } from 'expo-font';
+import { FIRESTORE_DB, FIREBASE_STORAGE } from '../firebaseConfig';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { IndividualItem } from '../components/IndividualItem';
+
 
 export const Current = () => {
   const [fontsLoaded] = useFonts({
     'Questrial-Regular': require('../assets/fonts/Questrial-Regular.ttf'),
   });
-  if (!fontsLoaded) {
-    return null;
-  }
+
+  const [datas, setDatas] = useState<any[]>([]); // Initialize with an empty array
+
+  useEffect(() => {
+    const querySnapshot = collection(FIRESTORE_DB, "current");
+
+    const subscriber = onSnapshot(querySnapshot, {
+      next: (snapshot) => {
+        const datas: any[] = [];
+        snapshot.docs.forEach((doc) => {
+          datas.push({
+            id: doc.id,
+            ...doc.data(),
+          })
+        });
+        setDatas(datas);
+      },
+    });
+
+    // console.log(datas)
+
+    return () => subscriber();
+  }, []);
+
 
   return (
-    <View>
-      <Text style={{ fontFamily: 'Questrial-Regular', fontSize: 40, padding:15 }}>Current</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <View>
+          <Text style={{ fontFamily: 'Questrial-Regular', fontSize: 40, padding:15 }}>Current</Text>
+        </View>
+
+        {datas.map((data) => (
+            <IndividualItem item={data}/> 
+          ))}
+
+      </View>
+    </ScrollView>
   )
 }
-
-  
