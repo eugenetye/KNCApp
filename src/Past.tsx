ee4dcbbb82cf4d897ce3c18256ca9a4102a7cf49
fleenorg <fleenorg@gmail.com>
2023-09-29 10:22:30 -0400

    sexy past page

--- src/Past.tsx
+++ src/Past.tsx
@@ -1,19 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, {useEffect, useState} from 'react'
 import { useFonts } from 'expo-font';
+import { FIRESTORE_DB, FIREBASE_STORAGE } from '../firebaseConfig';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { IndividualItem } from '../components/IndividualItem';
 
 export const Past = () => {
   const [fontsLoaded] = useFonts({
     'Questrial-Regular': require('../assets/fonts/Questrial-Regular.ttf'),
   });
-  if (!fontsLoaded) {
-    return null;
-  }
 
+
+  const [datas, setDatas] = useState<any[]>([]); // Initialize with an empty array
+
+
+  useEffect(() => {
+    const querySnapshot = collection(FIRESTORE_DB, "past");
+
+    const subscriber = onSnapshot(querySnapshot, {
+      next: (snapshot) => {
+        const datas: any[] = [];
+        snapshot.docs.forEach((doc) => {
+          datas.push({
+            id: doc.id,
+            ...doc.data(),
+          })
+        });
+        setDatas(datas);
+      },
+    });
+
+    // console.log(datas)
+
+    return () => subscriber();
+  }, []);
+  
+    
+  
   return (
-    <View>
-      <Text style={{ fontFamily: 'Questrial-Regular', fontSize: 40, padding:15 }}>Past</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <View>
+          <Text style={{ fontFamily: 'Questrial-Regular', fontSize: 40, padding:15 }}>Past</Text>
+        </View>
+
+        {datas.map((data) => (
+            <IndividualItem item={data}/> 
+          ))}
+
+      </View>
+    </ScrollView>
   )
 }
-
