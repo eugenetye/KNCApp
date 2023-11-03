36fc78e2a9e8ca2b69cd6d5fb6dd4d67b2d22eed
Eugene Tye <eugenetye123@gmail.com>
2023-11-03 09:23:20 -0400

    added trail_map

---
+++ src/Trail_Map.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CachedImage from '../components/CachedImage';
+import { WebView } from 'react-native-webview';
+
+export const Trail_Map = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <CachedImage url={'/trails/trail_map.jpeg'} style={styles.image} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
+});
+
